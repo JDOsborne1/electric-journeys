78d16ec391ba0bf4b2cb286b3b65253006429d93
JDO <atharos@vivaldi.net>
2020-07-11 22:04:57 +0100

    Adding some documentation

--- R/gmaps_wrappers.R
+++ R/gmaps_wrappers.R
@@ -2,6 +2,21 @@
 # Functions to wrap the gmaps functionality -------------------------------
 
 
+#' Get Metres Distance
+#'
+#' @description Using an origin-destination data frame, get the driving distance
+#'   in metres using the google maps API. Includes a default limiter to 10
+#'   queries to prevent accidental use of too many rows.
+#'
+#' @param orig_dest_df Data.frame/tibble/data.table of origin and destination
+#'   points. Postcodes are used in this project, but any legal input to the maps
+#'   API will work here
+#' @param smol Volume limiter switch, default TRUE
+#'
+#' @return
+#' @export
+#'
+#' @examples
 ejp_get_distance_metres <- function(orig_dest_df, smol = TRUE){
         if(smol){
                 used_links <- head(orig_dest_df, 10)
