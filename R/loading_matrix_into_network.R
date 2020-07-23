9fe2575d7100898db1e1b42cce666b2d695287ad
JDO <atharos@vivaldi.net>
2020-07-23 23:36:35 +0100

    Adding in a function to narrow the graph to its shortest path between two points

--- R/loading_matrix_into_network.R
+++ R/loading_matrix_into_network.R
@@ -18,3 +18,28 @@ ejp_PurGenerateNetwork <- function(.input_paths, .limit){
                 filter(Distance <= .limit) %>% 
                 as_tbl_graph(directed = FALSE)
 }
+
+#' Function to get the shortest path
+#'
+#' @description function to output the graph object which is the shortest path
+#'   from the origin to the destination, when considering the weights encoded in
+#'   the graph, in this case the distance in metres.
+#'
+#' @param .input_graph
+#' @param .origin The indicies of the node chosen as the origin
+#' @param .destin The indicies of the node chosen as the destination
+#'
+#' @return
+#' @export
+#'
+#' @examples
+ejp_PurGetShortestPath <- function(.input_graph, .origin, .destin){
+        node_names <- .input_graph %>%
+                activate(nodes) %>%
+                pull(name)
+        
+        from <- which(node_names == .origin)
+        to <-  which(node_names == .destin)
+        .input_graph %>% 
+                morph(to_shortest_path, from = from, to = to, weights = Distance)
+}
